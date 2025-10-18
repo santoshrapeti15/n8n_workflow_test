a98e86a4a080f1d86932d3606dfc527cffb0dabb
sr255098 <sr255098@teradata.com>
2025-10-18 21:02:40 +0530

    union_2 commit

--- union.sql
+++ union.sql
@@ -3,3 +3,5 @@ union
 sel id, customer, region, amount from raw_tbl.customer where code = 14
 union
 sel id, customer, region, amount from raw_tbl.customer where code = 16
+union
+sel id, customer, region, amount from raw_tbl.customer where code = 18
