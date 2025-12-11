f812601e71489c8da5d03a05d1f2aabef14aab6b
sr255098 <sr255098@teradata.com>
2025-12-11 20:50:00 +0530

    commit2

--- union.sql
+++ union.sql
@@ -5,3 +5,7 @@ union
 sel * from dbc.customer where customer_id = 16
 union
 sel * from dbc.customer where customer_id = 18
+union
+sel * from dbc.customer where customer_id = 20
+union
+sel * from dbc.customer where customer_id = 22
