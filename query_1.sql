42713c893bddafa1947bd861f3eea84bc46d4a82
sr255098 <sr255098@teradata.com>
2025-12-11 19:21:14 +0530

    commit

--- query_1.sql
+++
@@ -1,2 +0,0 @@
-sel id, name, start_date, last_update_date  from 
-(Sel * from cor_tbl.details) where id < 6;
--- union.sql
+++
@@ -1,7 +0,0 @@
-sel id, customer, region, amount from raw_tbl.customer where code = 12
-union
-sel id, customer, region, amount from raw_tbl.customer where code = 14
-union
-sel id, customer, region, amount from raw_tbl.customer where code = 16
-union
-sel id, customer, region, amount from raw_tbl.customer where code = 22
