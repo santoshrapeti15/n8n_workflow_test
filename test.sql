84b43aff25eedbe27c8a709049240d37a2150165
sr255098 <sr255098@teradata.com>
2025-12-11 19:49:00 +0530

    branch_2_commit_2

---
+++ test.sql
@@ -0,0 +1,7 @@
+sel id, customer, region, amount from raw_tbl.customer where code = 12
+union
+sel id, customer, region, amount from raw_tbl.customer where code = 14
+union
+sel id, customer, region, amount from raw_tbl.customer where code = 16
+union
+sel id, customer, region, amount from raw_tbl.customer where code = 22
