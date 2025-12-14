0919218a1884dcf2ccdf1a36709ed2f137a8bf52
sr255098 <sr255098@teradata.com>
2025-12-14 12:48:12 +0530

    commit

---
+++ test2.sql
@@ -0,0 +1,9 @@
+sel * from dbc.customer where customer_id = 12
+union
+sel * from dbc.customer where customer_id = 14
+union
+sel * from dbc.customer where customer_id = 16
+union
+sel * from dbc.customer where customer_id = 18
+union
+sel * from dbc.customer where customer_id = 20
